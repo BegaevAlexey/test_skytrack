f63ce79dd6365e365d7209a24fca5ac02c8a86b1
Alexey_B <ph.begaev@gmail.com>
2020-09-30 18:50:17 +0300

    FIX: fix minor

--- main.cpp
+++ main.cpp
@@ -59,6 +59,7 @@ int main(int argc, char **argv)
         }
 
         paramCallBack->currFrame = frame;
+        makeBlur(frame, paramCallBack.get())
 
         cv::imshow(NAME_WIN, frame);
         if (0 < cv::waitKey(pause))
