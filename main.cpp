ae06a40558904a723bbd8351e8b4c90537a598ad
Alexey_B <ph.begaev@gmail.com>
2020-09-30 18:55:49 +0300

    FIX: fix minor

--- main.cpp
+++ main.cpp
@@ -58,7 +58,7 @@ int main(int argc, char **argv)
             break;
         }
 
-        utls::makeBlur(frame, paramCallBack.get())
+        utls::makeBlur(frame, paramCallBack.get());
 
         cv::imshow(NAME_WIN, frame);
         if (0 < cv::waitKey(pause))
