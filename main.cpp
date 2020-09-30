ccc37d98dfa3ef0982f4deb56b8f9253fbb56f22
Alexey_B <ph.begaev@gmail.com>
2020-09-30 14:01:49 +0300

    IMPL: add plaing video

--- main.cpp
+++ main.cpp
@@ -17,8 +17,42 @@ int main(int argc, char **argv)
     {
         retrun -1;
     }
-    
 
+    // open video
+    std::string videoFile = utls::readCfgParam<std::string>("video_file");
+    cv::VideoCapture cap(videoFile);
+    if (!cap.isOpened())
+    {
+        std::cerr << "[ERROR] Video '" << videoFile << "'was not opened!" << std::endl;
+        return -1;
+    }
+
+    // calculate pause for waiting video
+    int fps = cap.get(cv::CV_CAP_PROP_FPS);
+    int pause = 1000 / fps; 
+
+    const std::string NAME_WIN = "SKYTRACK";
+    cv::namedWindow(NAME_WIN, cv::WINDOW_NORMAL);
+
+    cv::Mat frame;
+    while (true)
+    {
+        cap >> frame;
+        if(frame.isEmpty())
+        {
+            std::cout << "[INFO] Frame is empty!" << std::endl;
+            break;
+        }
+
+
+        cv::imshow(NAME_WIN, frame);
+        if (0 < cv::waitKey(pause))
+        {
+            break;
+        }
+    }
+
+    cv::destroyAllWindows();
 
     std::cout << "\n\t***END PROGRAM!***" << std::endl;
 }
